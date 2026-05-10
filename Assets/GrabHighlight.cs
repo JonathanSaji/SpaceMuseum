0d2d6a8ba4c503bf2a408a9b69b2df05b7cdf8e5
Jonathan Saji <jonathansaji2009@gmail.com>
2026-05-10 01:29:49 -0400

    idk

--- Assets/GrabHighlight.cs
+++ Assets/GrabHighlight.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class GrabHighlight : MonoBehaviour
 {
     public Color highlightColor = Color.yellow;
+    
     private Material mat;
     private Color originalColor;
-    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable interactable;
+    private XRSimpleInteractable interactable;
 
     void Start()
     {
         mat = GetComponentInChildren<Renderer>().material;
         originalColor = mat.color;
-        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable>();
+        interactable = GetComponent<XRSimpleInteractable>();
+
+        if (interactable == null)
+        {
+            Debug.LogError("No XRSimpleInteractable found on " + gameObject.name);
+            return;
+        }
 
         interactable.hoverEntered.AddListener(OnHover);
         interactable.hoverExited.AddListener(OnHoverExit);
@@ -20,11 +28,21 @@ public class GrabHighlight : MonoBehaviour
 
     void OnHover(HoverEnterEventArgs args)
     {
+        Debug.Log("Hovering: " + gameObject.name);
         mat.color = highlightColor;
     }
 
     void OnHoverExit(HoverExitEventArgs args)
     {
         mat.color = originalColor;
+    }
+
+    void OnDestroy()
+    {
+        if (interactable != null)
+        {
+            interactable.hoverEntered.RemoveListener(OnHover);
+            interactable.hoverExited.RemoveListener(OnHoverExit);
+        }
     }
 }
